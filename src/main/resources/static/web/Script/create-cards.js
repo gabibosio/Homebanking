7c371e0265b359d6cbce1a28cecf0e59d1feb76d
gabibosio <bosiogabriel94@gmail.com>
2022-06-28 15:01:44 -0300

    arregle sweet alert de create cards

--- src/main/resources/static/web/Script/create-cards.js
+++ src/main/resources/static/web/Script/create-cards.js
@@ -20,20 +20,7 @@ Vue.createApp({
             axios.post('https://homebakingmindhub.herokuapp.com/api/clients/current/cards',`type=${this.tipo}&color=${this.color}`,{headers:{'content-type': 'application/x-www-form-urlencoded'}})
             .then(response => window.location.href="https://homebakingmindhub.herokuapp.com/web/cards.html")
             .catch(error => {
-              if(error.response.data == "you have 3 cards of this type"){
-                Swal.fire({
-                  icon: 'error',
-                  title: 'Error',
-                  text: 'Ya tiene 3 Tarjetas de este tipo!',
-                })
-              }
-              if(error.response.data == "you have 1 card of this color"){
-                Swal.fire({
-                  icon: 'error',
-                  title: 'Error',
-                  text: 'Ya tiene 1 Tarjeta de este color!',
-                })
-              }
+              Swal.fire('Error!' , error.response.data, 'error')
               
             })
         }
